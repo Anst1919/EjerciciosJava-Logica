266fb6b47b4e1a27d2c213fde6b6a8fc9c968090
unknown <you@example.com>
2025-06-04 11:16:59 -0600

    Operaciones matemáticas

---
+++ OperacionesFaltantes.java
@@ -0,0 +1,9 @@
+public class OperacionesFaltantes {
+    void multiplicacion(double datoUno, double datoDos) {
+        System.out.println("El resultado de la multiplicacion es: " + datoUno*datoDos);
+    }
+
+    void división(double datoUno, double datoDos) {
+        System.out.println("El resultado de la división es: " + datoUno/datoDos);
+    }
+}
--- Suma.java
+++ Suma.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Suma {
+    void suma(int a, int b){
+        int suma = a + b;
+        System.out.println("El resultado de la operación es: " + suma);
+    }
 }
